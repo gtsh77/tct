5c2768dae899c414de06b2ff4136c2ca31f92c05
Anton Makridin <a.i.makridin@yandex.ru>
2017-10-04 08:04:50 +0300

    abi to provide def file, some new chks

--- src/main.c.ts
+++ src/main.c.ts
@@ -2,22 +2,47 @@
 
 interface rgba {
 	r: number;
-	b: number;
 	g: number;
+	b: number;
 	a: number;
+}
+
+interface definitions {
+	r: number;
+	g: number;
+	b: number;
+	id: number;
 }
 
 class Main {
 	private Jimp = require("jimp");
 	private fs = require("fs");
 	private mapArr: number[] = [];
+	private definitions: definitions[] = null;
 
 	public init(): void {
 		if(!process.argv[2]){
 			console.log('specify path to img file');
 			return;
 		}
+		else if(!process.argv[3]){
+			console.log('specify path to definitions file');
+			return;
+		}
+		this.fs.readFile(process.argv[3], (err, data) => {
+			if (err) throw err;
+			this.definitions = JSON.parse(data);
+			this.readImg();
+		});
+
+	}
+
+	public readImg(): void {
 		this.Jimp.read(process.argv[2],(err, image) => {
+			if(!image){
+				console.log('wrong img path');
+				return;
+			}
 		   	let width: number = image.bitmap.width;
 		   	let height: number = image.bitmap.height;
 		   	let arr: rgba[] = [];
@@ -28,15 +53,16 @@ class Main {
 		   		}
 		   	}
 		   	this.buildMap(arr);
-		});
+		});		
 	}
 
 	public buildMap(arr: rgba[]): void {
 		let mapSerial: string = null;
 		for(let pixel of arr){
-			if(pixel.a !== 0){
-				if(pixel.r === 255 && pixel.g === 255 && pixel.b === 255) this.mapArr.push(1);
-				else if(pixel.r === 0 && pixel.g === 0 && pixel.b === 0) this.mapArr.push(2);
+			if(pixel.a === 255){
+				for(let definition of this.definitions){
+					if(pixel.r === definition.r && pixel.g === definition.g && pixel.b === definition.b) this.mapArr.push(definition.id);
+				}
 			}
 			else this.mapArr.push(0);
 		}
